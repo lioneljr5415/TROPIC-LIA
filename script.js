657bfdb9c9fa8fad15f8f5d3988b1f51ae572e0f
lioneljr5415 <136764104+lioneljr5415@users.noreply.github.com>
2025-03-17 11:41:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
+    
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+        
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute
+('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+
+    })
+})
     const aumentaFonteBotao = document.getElementById
     ('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById
     ('diminuir-fonte');
+    const alternaContraste = document.getElementById
+    ('alterna-contraste')
   
     let tamanhoAtualFonte = 1;
 
@@ -16,12 +33,7 @@ document.addEventListener('DOMContentLoaded', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-        
-        const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-        
-        botaoDeAcessibilidade.addEventListener('click', function (){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-        })
-    })
+   
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
